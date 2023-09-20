2b335f99cd20be9cd6c27820a86e83f2ac08d37d
Ivan Nimych <in7264@gmail.com>
2023-09-20 12:59:02 +0300

    1.8v

--- code-encode3 copy.js
+++ code-encode3 copy.js
@@ -1,67 +1,69 @@
-function encrypt3() { 
-    var inputText = document.getElementById("inputText").value; 
-    var key = parseInt(document.getElementById("inputkey").value); 
-    var encryptedText = ""; 
- 
-    // Уберите пробелы из входного текста 
-    inputText = inputText.replace(/\s/g, ''); 
- 
-    // Вычислите длину текста 
-    var textLength = inputText.length; 
- 
-    // Вычислите количество столбцов в матрице 
-    var numColumns = Math.ceil(textLength / key); 
- 
-    // Создайте матрицу для зашифрованных символов 
-    var matrix = new Array(numColumns); 
-    for (var i = 0; i < numColumns; i++) { 
-        matrix[i] = []; 
-    } 
- 
-    // Заполните матрицу символами из входного текста 
-    var currentIndex = 0; 
-    for (var col = 0; col < numColumns; col++) { 
-        for (var row = 0; row < key; row++) { 
-            if (currentIndex < textLength) { 
-                matrix[col][row] = inputText[currentIndex]; 
-                currentIndex++; 
-            } else { 
-                matrix[col][row] = ""; 
-            } 
-        } 
-    } 
- 
-    // Считайте символы из матрицы в порядке столбцов 
-    for (var row = 0; row < key; row++) { 
-        for (var col = 0; col < numColumns; col++) { 
-            encryptedText += matrix[col][row]; 
-        } 
-    } 
- 
-    document.getElementById("outputText3").textContent = encryptedText; 
-} 
- 
-function decrypt3() {
-    var inputText = document.getElementById("inputText").value;
-    var key = parseInt(document.getElementById("inputkey").value);
-    var decryptedText = "";
+// Функція для шифрування методом шифру перестановки по стовпцях
+function encrypt3() {
+    var inputText = document.getElementById("inputText").value; // Отримуємо вхідний текст
+    var key = parseInt(document.getElementById("inputkey").value); // Отримуємо ключ для шифрування
+    var encryptedText = ""; // Змінна для збереження зашифрованого тексту
 
-    // Уберите пробелы из входного текста
+    // Видаляємо пробіли з вхідного тексту
     inputText = inputText.replace(/\s/g, '');
 
-    // Вычислите длину текста
+    // Визначаємо довжину тексту
     var textLength = inputText.length;
 
-    // Вычислите количество столбцов в матрице
+    // Визначаємо кількість стовпців у матриці
     var numColumns = Math.ceil(textLength / key);
 
-    // Создайте матрицу для расшифрованных символов
+    // Створюємо матрицю для зашифрованих символів
+    var matrix = new Array(numColumns);
+    for (var i = 0; i < numColumns; i++) {
+        matrix[i] = [];
+    }
+
+    // Заповнюємо матрицю символами з вхідного тексту
+    var currentIndex = 0;
+    for (var col = 0; col < numColumns; col++) {
+        for (var row = 0; row < key; row++) {
+            if (currentIndex < textLength) {
+                matrix[col][row] = inputText[currentIndex];
+                currentIndex++;
+            } else {
+                matrix[col][row] = "";
+            }
+        }
+    }
+
+    // Зчитуємо символи з матриці у порядку стовпців
+    for (var row = 0; row < key; row++) {
+        for (var col = 0; col < numColumns; col++) {
+            encryptedText += matrix[col][row];
+        }
+    }
+
+    document.getElementById("outputText3").textContent = encryptedText; // Виводимо зашифрований текст
+}
+
+// Функція для розшифрування методом шифру перестановки по стовпцях
+function decrypt3() {
+    var inputText = document.getElementById("inputText").value; // Отримуємо вхідний текст
+    var key = parseInt(document.getElementById("inputkey").value); // Отримуємо ключ для розшифрування
+    var decryptedText = ""; // Змінна для збереження розшифрованого тексту
+
+    // Видаляємо пробіли з вхідного тексту
+    inputText = inputText.replace(/\s/g, '');
+
+    // Визначаємо довжину тексту
+    var textLength = inputText.length;
+
+    // Визначаємо кількість стовпців у матриці
+    var numColumns = Math.ceil(textLength / key);
+
+    // Створюємо матрицю для розшифрованих символів
     var matrix = new Array(key);
     for (var i = 0; i < key; i++) {
         matrix[i] = [];
     }
 
-    // Заполните матрицу символами из входного текста
+    // Заповнюємо матрицю символами з вхідного тексту
     var currentIndex = 0;
     for (var row = 0; row < key; row++) {
         for (var col = 0; col < numColumns; col++) {
@@ -74,12 +76,12 @@ function decrypt3() {
         }
     }
 
-    // Считайте символы из матрицы в порядке столбцов
+    // Зчитуємо символи з матриці у порядку стовпців
     for (var col = 0; col < numColumns; col++) {
         for (var row = 0; row < key; row++) {
             decryptedText += matrix[row][col];
         }
     }
 
-    document.getElementById("outputText3").textContent = decryptedText;
+    document.getElementById("outputText3").textContent = decryptedText; // Виводимо розшифрований текст
 }
